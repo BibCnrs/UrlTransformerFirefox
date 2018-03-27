dfd782d94c1af0f5663229ca74cff0aa19be0301
fjoin <florence.join@gmail.com>
2018-03-27 16:31:03 +0200

    add menu on button to change domain

--- main.js
+++ main.js
@@ -10,3 +10,48 @@ async function handleClick () {
 
 browser.browserAction.onClicked.addListener(handleClick);
 
+var checkedState = true;
+const domains = [
+    'insb',
+    'inshs',
+    'inc',
+    'in2p3',
+    'inp',
+    'insmi',
+    'inee',
+    'insu',
+    'insis',
+    'ins2i',
+];
+
+browser.menus.create({
+    id: 'select',
+    title: 'select your domain',
+    type: 'radio',
+    contexts: ['browser_action']
+});
+
+domains.forEach(name => {
+    browser.menus.create({
+        id: name,
+        parentId: 'select',
+        type: 'radio',
+        title: name,
+        contexts: ['browser_action']
+    });
+});
+
+browser.storage.sync.get('domain').then(({ domain }) => {
+    if (!domain) {
+        return;
+    }
+    browser.menus.update(domain, { checked: true });
+});
+
+browser.menus.onClicked.addListener((info, tab, checked, wasChecked) => {
+    const name = info.menuItemId;
+    if (name === 'selected') {
+        return;
+    }
+    browser.storage.sync.set({ domain: name });
+});
