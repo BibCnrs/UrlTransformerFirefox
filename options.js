91e316ddac9904d204a360d041e9669780a4aa87
ThieryMichel <thiery@marmelab.com>
2018-03-28 17:30:43 +0200

    fix compatibility with chrome

--- options.js
+++ options.js
@@ -1,6 +1,6 @@
 (function () {
     const labelEl = document.querySelector('#label');
-    labelEl.innerHTML = browser.i18n.getMessage('choose-domain');
+    labelEl.innerHTML = browser.i18n.getMessage('choose_domain');
     const domainEl = document.querySelector('#domain');
     browser.storage.sync.get('domain').then(({ domain }) => {
         domainEl.value = domain;
